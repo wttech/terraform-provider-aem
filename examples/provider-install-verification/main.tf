7fbdc5bb9c3ac2dbb70cfd73b1c0435d27e8ff32
Krystian Panek <krystian.panek@wundermanthompson.com>
2023-08-25 21:52:59 +0200

    Tutorial resources

---
+++ examples/provider-install-verification/main.tf
@@ -0,0 +1,11 @@
+terraform {
+  required_providers {
+    aem = {
+      source = "registry.terraform.io/wttech/aem"
+    }
+  }
+}
+
+provider "aem" {}
+
+//data "hashicups_coffees" "example" {}
---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cat <<EOT >> ~/.terraformrc
+provider_installation {
+
+  dev_overrides {
+      "registry.terraform.io/wttech/aem" = "/Users/$(whoami)/go/bin"
+  }
+
+  # For all other providers, install them directly from their origin provider
+  # registries as normal. If you omit this, Terraform will _only_ use
+  # the dev_overrides block, and so no other providers will be available.
+  direct {}
+}
+EOT
+
+go install .
